66259121ff1543658d7e117457e1820f9bf84670
subahan ali <subahanislam523@gmail.com>
2024-10-03 01:36:19 +0600

    added to recap javascritp fundamentals

---
+++ fundamentals.js
@@ -0,0 +1,9 @@
+// How To Declare a variable user let or const 
+// const name  = "Subahan";
+let nameIs = 'Subahan';
+nameIs = "sultan";
+nameIs= "Rasel"
+console.log(nameIs)
+
+// 6 basic condition , > , < , <= , >= , === , !==  
+// Multiple condition , 
